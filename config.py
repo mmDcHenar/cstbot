903b4ef2017ac43e058e334c33b2bb41cc6062c6
mmDcHenar <mmDcHenar@gmail.com>
2024-11-15 21:20:57 +0330

    feat: add redis to project

--- config.py
+++ config.py
@@ -9,3 +9,6 @@ BASE_URL = config("BASE_URL", cast=str, default="https://example.com").strip("/"
 
 BOT_TOKEN = config("BOT_TOKEN", cast=str, default="")
 BOT_PROXY = config("BOT_PROXY", cast=str, default="") or None
+
+REDIS_HOST = config("REDIS_HOST", cast=str, default="redis")
+REDIS_PORT = config("REDIS_PORT", cast=int, default=6379)
