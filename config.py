1c0e05b6b8fdfdcf26f4b12de56eba2abb8f20aa
mmDcHenar <mmDcHenar@gmail.com>
2024-11-16 04:16:11 +0330

    feat: use Postgres as database

--- config.py
+++ config.py
@@ -12,3 +12,10 @@ BOT_PROXY = config("BOT_PROXY", cast=str, default="") or None
 
 REDIS_HOST = config("REDIS_HOST", cast=str, default="redis")
 REDIS_PORT = config("REDIS_PORT", cast=int, default=6379)
+
+POSTGRES_HOST = config("POSTGRES_HOST", cast=str, default="postgres")
+POSTGRES_PORT = config("POSTGRES_PORT", cast=int, default=5432)
+POSTGRES_USER = config("POSTGRES_USER", cast=str, default="root")
+POSTGRES_PASSWORD = config("POSTGRES_PASSWORD", cast=str, default="")
+POSTGRES_DB = config("POSTGRES_DB", cast=str, default="cst")
+
